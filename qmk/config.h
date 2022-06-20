7d922df1f5547a686127e5f820b7b7006aa82b8a
Pavel <pshabunia@gmail.com>
2022-06-20 18:34:28 -0500

    updated tapping config

--- qmk/config.h
+++ qmk/config.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#undef TAPPING_TERM
-#define TAPPING_TERM 100
-
 #define USE_SERIAL
 #define EE_HANDS
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
