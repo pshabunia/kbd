a2890d7ece8157e8399780195729f32f10716994
Pavel <pshabunia@gmail.com>
2022-10-26 23:19:34 -0500

    keymap extension v4

--- qmk/copy.sh
+++ qmk/copy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-PS34=$( ~/Downloads | grep ps34 | grep json | sort | tail -n 1)
+PS34=$(find ~/Downloads -type f -print | grep ps34 | grep json | sort | tail -n 1)
 cp -f $PS34 ps34.json
+
+# fix QK_BOOT
+sed -i '' -e 's/QK_BOOT/RESET/g' ps34.json
