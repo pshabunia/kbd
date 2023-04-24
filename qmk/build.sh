e0eb2cc69872f243f5acd82740650d4ad325931c
Pavel <pshabunia@gmail.com>
2023-04-23 23:19:49 -0500

    v10

--- qmk/build.sh
+++ qmk/build.sh
@@ -3,4 +3,6 @@ docker run --rm -ti \
 --volume $(pwd):/qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/pshabunia \
 --workdir /qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/pshabunia \
 qmkfm/qmk_firmware \
-/bin/bash -c 'qmk json2c ps34.json > keymap.c && qmk compile && cp /qmk_firmware/handwired_dactyl_manuform_5x6_pshabunia.hex keymap.hex'
+/bin/bash -c 'qmk json2c ps34.json > keymap.c \
+&& qmk compile \
+&& cp /qmk_firmware/handwired_dactyl_manuform_5x6_pshabunia.hex keymap.hex'
