4496ba357edf3fde18047e4f71aa9b163d84dc97
Pavel Sha <pshabunia@gmail.com>
2024-06-19 23:39:33 -0500

    code cleanup

--- qmk/config.h
+++ qmk/config.h
@@ -16,5 +16,5 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
-#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
-#define ONESHOT_TIMEOUT 3000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TAP_TOGGLE 3
+#define ONESHOT_TIMEOUT 3000
